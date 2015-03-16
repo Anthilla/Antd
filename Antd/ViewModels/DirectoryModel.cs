e7c3293cddab75025a2038d17c4e1c07d81d6439
zandam <damiano.zanardi@anthilla.com>
2015-03-16 17:56:08 +0100

    directory lister

---
+++ Antd/ViewModels/DirectoryModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Antd.ViewModels {
+    public class DirectoryModel {
+        public IEnumerable<string> parents { get; set; }
+        public IEnumerable<string> children { get; set; }
+    }
+}
