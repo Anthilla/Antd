7e8aa04a3a862a85c640d3b0e334c2ccb14a89ee
zandam <damiano.zanardi@anthilla.com>
2015-04-21 15:18:20 +0200

    add quartz to packages

---
+++ Antd/Scheduler/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UbearCore.Scheduler {
+    public class TaskModel {
+        [Key]
+        public string _Id { get; set; }
+
+        public string Guid { get; set; }
+
+        public string Data0 { get; set; }
+
+        public string Data1 { get; set; }
+
+        public int Interval { get; set; }
+    }
+}
---
+++ Antd/Scheduler/TaskRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UbearCore.Scheduler {
+    public class TaskRepository {
+        public static List<TaskModel> GetAll() {
+            List<TaskModel> list = DeNSo.Session.New.Get<TaskModel>().ToList();
+            return list;
+        }
+
+        public static void Create(string guid, string data0, string data1, int interval) {
+            TaskModel task = new TaskModel();
+            task._Id = Guid.NewGuid().ToString();
+            task.Guid = guid;
+            task.Data0 = data0;
+            task.Data1 = data1;
+            task.Interval = interval;
+            DeNSo.Session.New.Set(task);
+        }
+    }
+}
