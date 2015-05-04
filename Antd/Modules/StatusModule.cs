4726d1651a52869fd647821bd721d8a6c3ecab87
zandam <damiano.zanardi@anthilla.com>
2015-05-04 16:38:05 +0200

    unit page created

---
+++ Antd/Modules/StatusModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.Security;
+
+namespace Antd {
+
+    public class StatusModule : NancyModule {
+        public StatusModule()
+            : base("/status") {
+            this.RequiresAuthentication();
+
+            Get["/"] = x => {
+                return View["page-status"];
+            };
+        }
+    }
+}
