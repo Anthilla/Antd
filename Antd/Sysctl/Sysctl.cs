442657a5bc65c879d7648407d9857980cd7bc64b
zandam <damiano.zanardi@anthilla.com>
2015-05-05 11:26:57 +0200

    os status

---
+++ Antd/Sysctl/Sysctl.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Antd.Sysctl {
+    public class Sysctl {
+
+        private static List<SysctlModel> GetAllSysctls() {
+            CommandModel command = Command.Launch("sysctl", "--all");
+            var output = JsonConvert.SerializeObject(command.output);
+            List<SysctlModel> sysctls = MapSysctlJson(output);
+            return sysctls;
+        }
+
+        public static List<SysctlModel> All { get { return GetAllSysctls(); } }
+
+        public static List<SysctlModel> MapSysctlJson(string _sysctlJson) {
+            string sysctlJson = _sysctlJson;
+            sysctlJson = System.Text.RegularExpressions.Regex.Replace(_sysctlJson, @"\s{2,}", " ").Replace("\"", "").Replace("\\n", "\n");
+            string[] rowDivider = new String[] { "\n" };
+            string[] sysctlJsonRow = new string[] { };
+            sysctlJsonRow = sysctlJson.Split(rowDivider, StringSplitOptions.None).ToArray();
+            List<SysctlModel> sysctls = new List<SysctlModel>() { };
+            foreach (string rowJson in sysctlJsonRow) {
+                if (rowJson != null && rowJson != "") {
+                    string[] sysctlJsonCell = new string[] { };
+                    string[] cellDivider = new String[] { " = " };
+                    sysctlJsonCell = rowJson.Split(cellDivider, StringSplitOptions.None).ToArray();
+                    SysctlModel sysctl = MapSysctl(sysctlJsonCell);
+                    sysctls.Add(sysctl);
+                }
+            }
+            return sysctls;
+        }
+
+        public static SysctlModel MapSysctl(string[] _sysctlJsonCell) {
+            string[] sysctlJsonCell = _sysctlJsonCell;
+            SysctlModel sysctl = new SysctlModel();
+            sysctl.param = sysctlJsonCell[0];
+            sysctl.value = sysctlJsonCell[1];
+            return sysctl;
+        }
+    }
+}
