9d5bffb083518292bde4c8c2719ad3f1f7fd6519
zandam <damiano.zanardi@anthilla.com>
2015-05-07 11:38:04 +0200

    fixing sysctl status stable

---
+++ Antd/ViewHelpers/Status.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antd.ViewHelpers {
+    public class Status {
+        public static void Sysctl(List<SysctlModel> stockData, List<SysctlModel> runningData, List<SysctlModel> antdData) {
+            HashSet<string> paramNames = new HashSet<string>() { };
+            HashSet<string> valueNames = new HashSet<string>() { };
+
+            foreach (SysctlModel data in stockData) {
+                paramNames.Add(data.param);
+            }
+            foreach (SysctlModel data in runningData) {
+                paramNames.Add(data.param);
+            }
+            foreach (SysctlModel data in antdData) {
+                paramNames.Add(data.param);
+            }
+        }
+    }
+}
---
+++ Antd/ViewModels/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antd.ViewHelpers {
+    public class StatusViewModel {
+        public string label { get; set; }
+
+        public string stockValue { get; set; }
+
+        public string runningValue { get; set; }
+
+        public string antdValue { get; set; }
+    }
+}
