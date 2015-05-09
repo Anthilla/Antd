c08693794ce2b76d9d28263f992c37ab2014b51c
Heather <heather@live.ru>
2015-05-09 13:11:59 +0400

    Antd.Common/FileSystem and Antd/Common/Log
    Improvements to not crash on windows (Check if files exists before read), replace Console.WriteLine with ConsoleLogger.Log, reapply this patch: https://github.com/Anthilla/Antd/commit/29fa51a3a98b98c8cfbce413f3523615b857bb37

---
+++ Antd/Common/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antd.Common {
+    /// <summary>
+    /// Simple console Logger
+    /// </summary>
+    public static class ConsoleLogger {
+        public static void Log(string message, params object[] args) {
+            if (args.Count() > 0) message = String.Format(message, args);
+            Console.WriteLine("{0}{1}"
+                , ConsoleTime.GetTime(DateTime.Now)
+                , message);
+        }
+        public static void Warn(string message, params object[] args) {
+            if (args.Count() > 0) message = String.Format(message, args);
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("{0}Warn: {1}"
+                , ConsoleTime.GetTime(DateTime.Now)
+                , message);
+            Console.ForegroundColor = currentColor;
+        }
+        public static void Error(string message, params object[] args) {
+            if (args.Count() > 0) message = String.Format(message, args);
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("{0}Error: {1}"
+                , ConsoleTime.GetTime(DateTime.Now)
+                , message);
+            Console.ForegroundColor = currentColor;
+            Environment.Exit(-1);
+        }
+    }
+}
