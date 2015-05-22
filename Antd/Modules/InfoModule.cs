5b5a9d85af47032609ac9c8da83ce5806585c55b
zandam <damiano.zanardi@anthilla.com>
2015-05-22 09:56:23 +0200

    up to date

---
+++ Antd/Modules/InfoModule.cs
@@ -0,0 +1,19 @@
+﻿using Antd.Status;
+using Nancy;
+
+namespace Antd.Modules {
+    public class InfoModule : NancyModule {
+
+        public InfoModule()
+            : base("/info") {
+
+            Get["/loadaverage"] = x => {
+                return Response.AsJson(Uptime.LoadAverage);
+            };
+
+            Get["/disk"] = x => {
+                return Response.AsJson(Uptime.LoadAverage);
+            };
+        }
+    }
+}
