ba2f7d10bef32a69080863d03f7a4345eedba697
zandam <damiano.zanardi@anthilla.com>
2015-05-22 12:09:39 +0200

    code cleanup

--- Antd/Common/Log.cs
+++ Antd/Common/Log.cs
@@ -38,7 +38,7 @@ namespace Antd.Common {
         public static void Warn(string message, params object[] args) {
             if (args.Count() > 0) message = String.Format(message, args);
             ConsoleColor currentColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("{0}Warn: {1}"
                 , ConsoleTime.GetTime(DateTime.Now)
                 , message);
@@ -48,7 +48,7 @@ namespace Antd.Common {
         public static void Error(string message, params object[] args) {
             if (args.Count() > 0) message = String.Format(message, args);
             ConsoleColor currentColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("{0}Error: {1}"
                 , ConsoleTime.GetTime(DateTime.Now)
                 , message);
