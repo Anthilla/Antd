c64b6151b7baa6315a37fd4478b69afe078e5e3a
zandam <damiano.zanardi@anthilla.com>
2015-06-02 22:27:26 +0200

    storage and firewall views

---
+++ Antd/Modules/FirewallModule.cs
@@ -0,0 +1,65 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace Antd {
+
+    public class FirewallModule : NancyModule {
+
+        public FirewallModule()
+            : base("/firewall") {
+            this.RequiresAuthentication();
+
+            Get["/home"] = x => {
+                return View["page-firewall-home"];
+            };
+
+            Get["/manage"] = x => {
+                return View["page-firewall-mgmt"];
+            };
+
+            Get["/util"] = x => {
+                return View["page-firewall-util"];
+            };
+
+            Get["/hspot"] = x => {
+                return View["page-firewall-hspot"];
+            };
+
+            Get["/filter"] = x => {
+                return View["page-firewall-filter"];
+            };
+
+            Get["/mail"] = x => {
+                return View["page-firewall-mail"];
+            };
+
+            Get["/mail/domains"] = x => {
+                return View["page-firewall-mldmns"];
+            };
+
+            Get["/user/acl"] = x => {
+                return View["page-firewall-user"];
+            };
+
+            Get["/network"] = x => {
+                return View["page-firewall-net"];
+            };
+
+            Get["/network/server"] = x => {
+                return View["page-firewall-netsrv"];
+            };
+
+            Get["/network/vpn"] = x => {
+                return View["page-firewall-vpn"];
+            };
+
+            Get["/log"] = x => {
+                return View["page-firewall-log"];
+            };
+
+            Get["/setup"] = x => {
+                return View["page-firewall-setup"];
+            };
+        }
+    }
+}
---
+++ Antd/Modules/StorageModule.cs
@@ -0,0 +1,61 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace Antd {
+
+    public class StorageModule : NancyModule {
+
+        public StorageModule()
+            : base("/storage") {
+            this.RequiresAuthentication();
+
+            Get["/home"] = x => {
+                return View["page-storage-home"];
+            };
+
+            Get["/manage/disks"] = x => {
+                return View["page-storage-disk"];
+            };
+
+            Get["/manage/iscsi"] = x => {
+                return View["page-storage-iscsi"];
+            };
+
+            Get["/sharing"] = x => {
+                return View["page-storage-share"];
+            };
+
+            Get["/user/acl"] = x => {
+                return View["page-storage-user"];
+            };
+
+            Get["/backup"] = x => {
+                return View["page-storage-backup"];
+            };
+
+            Get["/mail"] = x => {
+                return View["page-storage-mail"];
+            };
+
+            Get["/mail/domains"] = x => {
+                return View["page-storage-mldmns"];
+            };
+
+            Get["/log"] = x => {
+                return View["page-storage-log"];
+            };
+
+            Get["/network"] = x => {
+                return View["page-storage-net"];
+            };
+
+            Get["/network/server"] = x => {
+                return View["page-storage-netsrv"];
+            };
+
+            Get["/setup"] = x => {
+                return View["page-storage-setup"];
+            };
+        }
+    }
+}
