33cca35d4aac67f7fef9c21f8dea4e9f5e0b1865
zandam <damiano.zanardi@anthilla.com>
2015-06-09 10:00:55 +0200

    add license reference

--- build/Content/Scripts/antd.js
+++ build/Content/Scripts/antd.js
@@ -1,3 +1,24 @@
+//global?////////////////////////////////////////////////////////////////////////////////////////
+$('p[id^=Update]').click(function () {
+    var self = $(this);
+    var id = self.attr('id');
+    var inputID = id.replace('Update', 'New');
+    var newValue = $('#' + inputID).val();
+    if (newValue.length > 0) {
+        jQuery.support.cors = true;
+        $.ajax({
+            url: '/command/mgmt/ex/' + inputID + '/' + newValue,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json;charset=utf-8',
+            success: function (data) {
+                console.log(data);
+                return false;
+            }
+        });
+    }
+});
+
 //page-system-general
 $('#NewHostname').keyup(function () {
     var value = $('#NewHostname').val();
