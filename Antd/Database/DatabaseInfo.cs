809a6a8815c7c385fb92203069c81cb8e22fa47a
zandam <damiano.zanardi@anthilla.com>
2015-06-23 17:27:38 +0200

    database information

---
+++ Antd/Database/DatabaseInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antd.Database {
+    public class DatabaseInfo {
+        public static string GetJnlFilePath() {
+            var applicationRoot = AppDomain.CurrentDomain.BaseDirectory;
+            var files = Directory.GetFiles(applicationRoot, "*.jnl", SearchOption.AllDirectories);
+            return string.Join(", ", files).ToString();
+        }
+
+        public static string GetVersion() {
+            return DeNSo.Configuration.Version.ToString();
+        }
+    }
+}
