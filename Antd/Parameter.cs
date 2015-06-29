fa4dc9a7065660bc2a3e7c60adb08aa4b57f50f6
zandam <damiano.zanardi@anthilla.com>
2015-06-29 18:54:09 +0200

    fixed constant paths

---
+++ Antd/Parameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace Antd {
+
+    public class Folder {
+        public static string Root {get{return AppDomain.CurrentDomain.BaseDirectory;}}
+        public static string Config { get { return Path.Combine(Folder.Root, "config"); } }
+        public static string Database { get { return Path.Combine(Folder.Root, "database"); } }
+        public static string Networkd { get { return Path.Combine(Folder.Root, "networkd"); } }
+    }
+}
