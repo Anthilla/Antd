63696cc4e1ebedd55be55234a76c1424f408fc56
zandam <damiano.zanardi@anthilla.com>
2015-06-30 18:10:10 +0200

    terminal

--- build/Content/Scripts/antd.js
+++ build/Content/Scripts/antd.js
@@ -1,3 +1,84 @@
+//terminal
+$('#OpenTerminal').click(function () {
+    $('#Terminal').toggle();
+    $('#TerminalContent').toggle();
+});
+
+jQuery(function ($) {
+    $('#TerminalContent').terminal(function (cmd, term) {
+        if (cmd == 'help') {
+            term.echo("available commands are mysql, js, test");
+        } else if (cmd == 'test') {
+            term.push(function (cmd, term) {
+                if (command == 'help') {
+                    term.echo('type "ping" it will display "pong"');
+                } else if (cmd == 'ping') {
+                    term.echo('pong');
+                } else {
+                    term.echo('unknown command "' + cmd + '"');
+                }
+            }, {
+                prompt: 'test> ',
+                name: 'test'
+            });
+        } else if (command == "js") {
+            term.push(function (command, term) {
+                var result = window.eval(command);
+                if (result != undefined) {
+                    term.echo(String(result));
+                }
+            }, {
+                name: 'js',
+                prompt: 'js> '
+            });
+        } else if (command == 'mysql') {
+            term.push(function (command, term) {
+                term.pause();
+                //$.jrpc is helper function which
+                //creates json-rpc request
+                $.jrpc("mysql-rpc-demo.php",
+                  "query",
+                  [command],
+                  function (data) {
+                      term.resume();
+                      if (data.error) {
+                          term.error(data.error.message);
+                      } else {
+                          if (typeof data.result == 'boolean') {
+                              term.echo(data.result ?
+                                        'success' :
+                                        'fail');
+                          } else {
+                              var len = data.result.length;
+                              for (var i = 0; i < len; ++i) {
+                                  term.echo(data.result[i].join(' | '));
+                              }
+                          }
+                      }
+                  },
+                  function (xhr, status, error) {
+                      term.error('[AJAX] ' + status +
+                                 ' - Server reponse is: \n' +
+                                 xhr.responseText);
+                      term.resume();
+                  }); // rpc call
+            }, {
+                greetings: "This is example of using mysql" +
+                  " from terminal\n you are allowed to exe" +
+                  "cute: select, insert, update and delete" +
+                  " from/to table:\n   table test(integer_" +
+                  "value integer, varchar_value varchar(255))",
+                prompt: "mysql> "
+            });
+        } else {
+            term.echo("unknow command " + command);
+        }
+    }, {
+        greetings: "multiply terminals demo use help" +
+              " to see available commands"
+    });
+});
+
 ///command management
 $('#CmdMgmtButton').click(function () {
     $('#CommandManagementForm').toggle();
