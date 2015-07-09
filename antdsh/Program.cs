2a9975d1070384bfc8c253439b0790d405b90e91
zandam <damiano.zanardi@anthilla.com>
2015-07-09 14:40:51 +0200

    shell

---
+++ antdsh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antdsh {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("> antdsh");
+            Console.WriteLine("> {0} args", args.Length);
+            if (args.Length == 0) {
+                args[0] = Console.ReadLine();
+                Command(args[0]);
+            }
+            else {
+                Command(args[0]);
+            }
+            Console.ReadLine();
+        }
+
+        static void Command(string command) {
+            Console.WriteLine("> " + command);
+            if (command == "check-update") {
+                shell.CheckUpdate();
+            }
+        }
+    }
+}
---
+++ antdsh/shell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antdsh {
+    public class shell {
+        public static void Info() { }
+
+        public static void CheckUpdate() { }
+
+        public static void Update() { }
+
+        public static void ForceUpdate() { }
+    }
+}
