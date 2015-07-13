3c50cf8364f8fa418f0b44487c599a360fd977a5
zandam <damiano.zanardi@anthilla.com>
2015-07-13 17:46:07 +0200

    terminal

--- Antd/Modules/TerminalModule.cs
+++ Antd/Modules/TerminalModule.cs
@@ -48,7 +48,7 @@ namespace Antd {
             Post["/"] = x => {
                 string cmd = this.Request.Form.Command;
                 string result = (this.Request.Form.Directory == "") ? Terminal.Execute(cmd) : Terminal.Execute(cmd, this.Request.Form.Directory);
-                FileSystem.WriteFile("/mnt/cdrom/pippo", result);
+                FileSystem.WriteFile("/mnt/cdrom/pippo.txt", result);
                 return Response.AsJson(result);
             };
 
