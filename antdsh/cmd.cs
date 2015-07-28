e9800c09692ac39165a68aee8805109260f1793f
zandam <damiano.zanardi@anthilla.com>
2015-07-28 15:20:54 +0200

    shell

--- antdsh/cmd.cs
+++ antdsh/cmd.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace antdsh {
+﻿namespace antdsh {
     public class cmd {
         public string timestamp { get; set; }
 
