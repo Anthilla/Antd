9f4ea7d411994fbb6c8925092b29fd7ff32b7f5d
zandam <damiano.zanardi@anthilla.com>
2015-08-20 16:19:28 +0200

    srvs

---
+++ antdlib/Svcs/Samba/SambaCongif.cs
@@ -0,0 +1,77 @@
+﻿
+using antdlib.MountPoint;
+///-------------------------------------------------------------------------------------
+///     Copyright (c) 2014, Anthilla S.r.l. (http://www.anthilla.com)
+///     All rights reserved.
+///
+///     Redistribution and use in source and binary forms, with or without
+///     modification, are permitted provided that the following conditions are met:
+///         * Redistributions of source code must retain the above copyright
+///           notice, this list of conditions and the following disclaimer.
+///         * Redistributions in binary form must reproduce the above copyright
+///           notice, this list of conditions and the following disclaimer in the
+///           documentation and/or other materials provided with the distribution.
+///         * Neither the name of the Anthilla S.r.l. nor the
+///           names of its contributors may be used to endorse or promote products
+///           derived from this software without specific prior written permission.
+///
+///     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+///     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+///     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+///     DISCLAIMED. IN NO EVENT SHALL ANTHILLA S.R.L. BE LIABLE FOR ANY
+///     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+///     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+///     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+///     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+///     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+///     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
+///     20141110
+///-------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antdlib.Svcs.Samba {
+    public class SambaConfig {
+
+        private static string dir = "/etc/samba";
+
+        private static string DIR = Mount.SetDIRSPath(dir);
+
+        private static string mainFile = "smb.conf";
+
+        public class Model {
+            public string _Id { get; set; }
+
+            public string Guid { get; set; }
+
+            public List<string> ServiceStructure { get; set; } = new List<string>() { };
+
+        }
+
+        public static void SetReady() {
+            Terminal.Execute($"cp {dir} {DIR}");
+            FileSystem.CopyDirectory(dir, DIR);
+            Mount.Dir(dir);
+        }
+
+        public static List<string> GetServiceStructure() {
+            var list = new List<string>() { };
+            var files = Directory.EnumerateFiles(DIR, "*.conf", SearchOption.AllDirectories).ToArray();
+            for (int i = 0; i < files.Length; i++) {
+                if (File.ReadLines(files[i]).Any(line => line.Contains("include"))) {
+                    list.Add(files[i]);
+                }
+            }
+            if (list.Count() < 1) {
+                list.Add($"{DIR}/{mainFile}");
+            }
+            return list;
+        }
+    }
+}
