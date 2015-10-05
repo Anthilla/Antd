daf7faaa6db3147b760fba3ec12d94a8327ec219
zandam <damiano.zanardi@anthilla.com>
2015-10-05 16:36:05 +0200

    autoupdate

--- autoupdate/autoupdate.sh
+++ autoupdate/autoupdate.sh
@@ -8,15 +8,16 @@ echo Installing Antd...
 mkdir -p /mnt/cdrom/Apps/tmp
 mount -t tmpfs tmpfs /mnt/cdrom/Apps/tmp
 wget "http://srv.anthilla.com:8081/antdsh-update/update.txt" -O /mnt/cdrom/Apps/tmp/update.txt
-version= tail -1 /mnt/cdrom/Apps/tmp/update.txt
-wget "http://srv.anthilla.com:8081/antdsh-update/update.txt/$version"
+VERSION= "$(tail -1 /mnt/cdrom/Apps/tmp/update.txt)"
+echo "version found: $VERSION"
+wget "http://srv.anthilla.com:8081/antdsh-update/update.txt/$VERSION"
 mkdir -p /framework/antdsh
 mkdir -p /mnt/cdrom/Apps/Anthilla_antdsh
 umount -t tmpfs /mnt/cdrom/Apps/tmp
 rm -fR /mnt/cdrom/Apps/tmp
 
 echo Linking newest version
-ln -s /mnt/cdrom/Apps/Anthilla_antdsh/$version /mnt/cdrom/Apps/Anthilla_antdsh/active-version
+ln -s /mnt/cdrom/Apps/Anthilla_antdsh/$VERSION /mnt/cdrom/Apps/Anthilla_antdsh/active-version
 
 echo Mounting antdsh
 mount /mnt/cdrom/Apps/Anthilla_antdsh/active-version /framework/antdsh
