24ad472254bf7bbc180aea1bc23df06e5d6898a5
Grul- <damiano.zanardi@anthilla.com>
2017-02-06 12:48:16 +0100

    antdui: apps context views

--- AntdUi/Scripts/controller-context-apps.js
+++ AntdUi/Scripts/controller-context-apps.js
@@ -3,6 +3,14 @@
 app.controller("AppsDetectController", ["$scope", "$http", AppsDetectController]);
 
 function AppsDetectController($scope, $http) {
+    $scope.setup = function (appName) {
+        var data = $.param({
+            AppName: appName
+        });
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http.post("/apps/setup", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
+    }
+
     $http.get("/apps/detect").success(function (data) {
         $scope.AppsDetect = data;
     });
@@ -11,41 +19,82 @@ function AppsDetectController($scope, $http) {
 app.controller("AppsManagementController", ["$scope", "$http", AppsManagementController]);
 
 function AppsManagementController($scope, $http) {
-    $scope.stop = function (appName) {
+    $scope.ShowLog = function (unit) {
+        $http.get("/services/log?unit=" + unit.Name).success(function (data) {
+            unit.LogHtml = $sce.trustAsHtml(data);
+            unit.Hide = false;
+        });
+    }
+
+    $scope.Start = function (unit) {
         var data = $.param({
-            Name: appName
+            Unit: unit.Name
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-        $http.post("/apps/stop", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
-    }
+        $http.post("/services/start", data).then(
+            function () {
+                alert("Ok!");
+            },
+        function (response) {
+            console.log(response);
+        });
+    };
 
-    $scope.restart = function (appName) {
+    $scope.Restart = function (unit) {
         var data = $.param({
-            Name: appName
+            Unit: unit.Name
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-        $http.post("/apps/restart", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
-    }
-
-    $scope.AppIsActive = function (appName) {
-        $http.get("/apps/active/" + appName).success(function (data) {
-            return data;
+        $http.post("/services/restart", data).then(
+            function () {
+                alert("Ok!");
+            },
+        function (response) {
+            console.log(response);
         });
-    }
+    };
 
-    $scope.AppStatus = function (appName) {
-        $http.get("/apps/status/" + appName).success(function (data) {
-            return data;
-        });
-    }
-
-    $scope.setup = function (appName) {
+    $scope.Stop = function (unit) {
         var data = $.param({
-            AppName: appName
+            Unit: unit.Name
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-        $http.post("/apps/setup", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
-    }
+        $http.post("/services/stop", data).then(
+            function () {
+                alert("Ok!");
+            },
+        function (response) {
+            console.log(response);
+        });
+    };
+
+    $scope.Enable = function (unit) {
+        var data = $.param({
+            Unit: unit.Name
+        });
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http.post("/services/enable", data).then(
+            function () {
+                alert("Ok!");
+            },
+        function (response) {
+            console.log(response);
+        });
+    };
+
+    $scope.Disable = function (unit) {
+        var data = $.param({
+            Unit: unit.Name
+        });
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http.post("/services/disable", data).then(
+            function () {
+                alert("Ok!");
+            },
+        function (response) {
+            console.log(response);
+        });
+    };
 
     $http.get("/apps/management").success(function (data) {
         $scope.Apps = data;
