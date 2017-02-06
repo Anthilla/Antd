a660a3b7eb7cc8786d547cdcb36985a281367967
Grul- <damiano.zanardi@anthilla.com>
2017-02-06 15:00:59 +0100

    antdui: html pages

--- AntdUi/Scripts/controller-context-boot.js
+++ AntdUi/Scripts/controller-context-boot.js
@@ -20,63 +20,65 @@ function BootCommandsController($scope, $http) {
 app.controller("BootModulesController", ["$scope", "$http", BootModulesController]);
 
 function BootModulesController($scope, $http) {
-    $scope.saveModblacklist = function (config) {
+    $scope.saveModblacklist = function () {
         var data = $.param({
-            Config: config
+            Config: $scope.Blacklist
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post("/boot/modblacklist", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
     }
 
-    $scope.saveRmmodules = function (config) {
+    $scope.saveRmmodules = function () {
         var data = $.param({
-            Config: config
+            Config: $scope.RmModules
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post("/boot/rmmodules", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
     }
 
-    $scope.saveModules = function (config) {
+    $scope.saveModules = function () {
         var data = $.param({
-            Config: config
+            Config: $scope.Modules
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post("/boot/modules", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
     }
 
     $http.get("/boot/modules").success(function (data) {
-        $scope.Modules = data;
+        $scope.Modules = data.Modules;
+        $scope.RmModules = data.RmModules;
+        $scope.Blacklist = data.Blacklist;
     });
 }
 
 app.controller("BootOsParametersController", ["$scope", "$http", BootOsParametersController]);
 
 function BootOsParametersController($scope, $http) {
-    $scope.saveModules = function (config) {
+    $scope.save = function () {
         var data = $.param({
-            Config: config
+            Config: $scope.OsParameters
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post("/boot/osparameter", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
     }
 
     $http.get("/boot/osparameter").success(function (data) {
-        $scope.OsParameters = data;
+        $scope.OsParameters = data.OsParameters;
     });
 }
 
 app.controller("BootServicesController", ["$scope", "$http", BootServicesController]);
 
 function BootServicesController($scope, $http) {
-    $scope.saveModules = function (config) {
+    $scope.save = function () {
         var data = $.param({
-            Config: config
+            Config: $scope.Services
         });
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         $http.post("/boot/services", data).then(function () { alert("Ok!"); }, function (r) { console.log(r); });
     }
 
     $http.get("/boot/services").success(function (data) {
-        $scope.Services = data;
+        $scope.Services = data.Services;
     });
 }
