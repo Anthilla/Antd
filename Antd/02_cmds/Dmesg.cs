cd7e0af8fa6a33444cb296d163042bcdc1b48c1e
Grul- <damiano.zanardi@anthilla.com>
2017-10-18 12:17:33 +0200

    Add dmesg consultation methods and http routes

---
+++ Antd/02_cmds/Dmesg.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using anthilla.core;
+
+namespace Antd.cmds {
+    public class Dmesg {
+
+        private const string dmesgFileLocation = "/bin/dmesg";
+
+        public static string[] GetLog() {
+            return CommonProcess.Execute(dmesgFileLocation).ToArray();
+        }
+    }
+}
---
+++ AntdUi/04_modules/DmesgModule.cs
@@ -0,0 +1,16 @@
+﻿using anthilla.core;
+using Nancy;
+using Nancy.Security;
+
+namespace AntdUi.Modules {
+    public class DmesgModule : NancyModule {
+
+        public DmesgModule() : base("/dmesg") {
+            this.RequiresAuthentication();
+
+            Get["/"] = x => {
+                return ApiConsumer.GetJson(CommonString.Append(Application.ServerUrl, Request.Path));
+            };
+        }
+    }
+}
