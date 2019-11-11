82e30b6db19d9826e3feb62d97699d6c2dcda2af
zandam <damiano.zanardi@anthilla.com>
2019-11-11 16:44:14 +0100

    rimosso progetti inutilizzati

--- AntdUi/Scripts/controller-login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var app = angular.module("loginApp", ["pascalprecht.translate"]);
-
-app.config(function ($translateProvider) {
-    $translateProvider.useUrlLoader("/translate");
-    $translateProvider.preferredLanguage("it");
-});
