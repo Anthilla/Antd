e16d7598853615d6493726c61bc7695f0a4955d0
zandam <damiano.zanardi@anthilla.com>
2019-11-11 17:40:24 +0100

    static content conventions

--- AntdUi2/Web/AntdUiBootstrapper.cs
+++ AntdUi2/Web/AntdUiBootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.TinyIoc;
 
 namespace AntdUi2.Web {
@@ -8,6 +9,26 @@ namespace AntdUi2.Web {
 
         public AntdUiBootstrapper() {
         }
+
+        protected override void ConfigureConventions(NancyConventions conv) {
+            base.ConfigureConventions(conv);
+            conv.StaticContentsConventions.Clear();
+            conv.StaticContentsConventions.AddDirectory("css", @"/Style");
+            conv.StaticContentsConventions.AddDirectory("js", @"/Scripts");
+            conv.StaticContentsConventions.AddDirectory("fonts", @"/Fonts");
+            conv.StaticContentsConventions.AddDirectory("webfonts", @"/FontsWeb");
+            conv.StaticContentsConventions.AddDirectory("img", @"/Images");
+            conv.StaticContentsConventions.AddDirectory("part", @"/Views/partials");
+            conv.StaticContentsConventions.AddDirectory("shared", @"/Views/shared");
+            conv.StaticContentsConventions.AddDirectory("static", @"/Views/static");
+            conv.StaticContentsConventions.AddDirectory("pg", @"/Views/pages");
+            conv.StaticContentsConventions.AddDirectory("lo", @"/Views/layouts");
+            conv.StaticContentsConventions.AddDirectory("pt", @"/Views/partial");
+            conv.StaticContentsConventions.AddDirectory("tpl", @"/Views/templates");
+            conv.StaticContentsConventions.AddDirectory("prnt", @"/Views/print");
+            conv.StaticContentsConventions.AddDirectory("novnc", @"/novnc");
+        }
+
 
         protected override void ConfigureApplicationContainer(TinyIoCContainer container) {
             base.ConfigureApplicationContainer(container);
