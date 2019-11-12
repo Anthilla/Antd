73bab5577352b02153ea76ae18afa90ebcceb93e
zandam <damiano.zanardi@anthilla.com>
2019-11-12 15:33:43 +0100

    integrato comandi fdisk base

--- Antd2/01_core/commands/DiskCommand.cs
+++ Antd2/01_core/commands/DiskCommand.cs
@@ -8,9 +8,11 @@ namespace Antd2 {
         public static readonly Dictionary<string, Action<string[]>> Options =
             new Dictionary<string, Action<string[]>> {
                 {"show", Show },
+                {"create", Create },
+                {"delete", Delete },
             };
 
-        static void Show(string[] args) {
+        private static void Show(string[] args) {
             var blkid = Blkid.Get();
             foreach (var disk in blkid) {
                 Console.WriteLine($"  {disk.Partition}\t{disk.Type} {disk.Uuid} {disk.Label}");
@@ -27,6 +29,28 @@ namespace Antd2 {
             foreach (var disk in lsblk) {
                 Console.WriteLine($"  {disk.Name}\t{disk.Size} at {disk.Mountpoint}");
             }
+        }
+
+        private static void Create(string[] args) {
+            if (args.Length < 2) {
+                Console.WriteLine("  not enough arguments");
+                return;
+            }
+            var disk = args[0];
+            var size = args[1];
+            Console.WriteLine($"  create new partition on {disk} ({size})");
+            Fdisk.CreatePrimaryPartition(disk, size);
+        }
+
+        private static void Delete(string[] args) {
+            if (args.Length < 2) {
+                Console.WriteLine("  not enough arguments");
+                return;
+            }
+            var disk = args[0];
+            var number = args[1];
+            Console.WriteLine($"  delete partition {disk}{number}");
+            Fdisk.DeletePartition(disk, number);
         }
     }
 }
---
+++ Antd2/02_cmds/Fdisk.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Antd2.cmds {
+
+    /// <summary>
+    /// https://linux.die.net/man/8/fdisk
+    /// </summary>
+    public class Fdisk {
+
+        private const string fdiskCommand = "fdisk";
+        private const char eCR = '\n';      //corrisponde a 'Invio' - conferma il comando/opzione
+        private const char eN = 'n';        //add a new partition
+        private const char eP = 'p';        //print partition table O   primary type se dopo 'n'
+        private const char eE = 'e';        //                          exended type se dopo 'n'
+        private const char eW = 'w';        //write table to disk and exit
+        private const char eD = 'd';        //delete a partition
+
+        public static IEnumerable<string> GetPartitionTable(string device) {
+            var lines = Bash.Execute($"{fdiskCommand} -l {device}");
+            return lines;
+        }
+
+        /// <summary>
+        /// "n \n p \n 1 \n \n +300MB \n w \n"
+        /// </summary>
+        /// <param name="device"></param>
+        public static void CreatePrimaryPartition(string device, string partitionSize) {
+            var cmd = $"echo -e \\\"{eN}{eCR}{eP}{eCR}{eCR}{eCR}{partitionSize}{eCR}{eW}{eCR}\\\" | {fdiskCommand} {device}";
+            Console.WriteLine("  " + cmd);
+            var lines = Bash.Execute(cmd);
+            foreach(var line in lines) {
+                Console.WriteLine("    " + line);
+            }
+        }
+
+        public static void DeletePartition(string device, string number) {
+            var cmd = $"echo -e \\\"{eD}{eCR}{number}{eCR}{eW}{eCR}\\\" | {fdiskCommand} {device}";
+            Bash.Do(cmd);
+        }
+
+        private static (string Name, string MajMin, string Rm, string Size, string Ro, string Type, string Mountpoint) ParseLsblkLine(string line) {
+            var arr = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var mp = arr.Length < 7 ? string.Empty : arr[6];
+            return (arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], mp);
+        }
+    }
+}
