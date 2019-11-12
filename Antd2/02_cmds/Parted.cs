b480c66231129a31288d58d23ebc0a9945a4e6d4
zandam <damiano.zanardi@anthilla.com>
2019-11-12 15:49:00 +0100

    integrato comando parted

---
+++ Antd2/02_cmds/Parted.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Antd2.cmds {
+
+    /// <summary>
+    /// https://linux.die.net/man/8/parted
+    /// </summary>
+    public class Parted {
+
+        private const string partedCommand = "parted";
+        private const char eCR = '\n';      //corrisponde a 'Invio' - conferma il comando/opzione
+
+        public static IEnumerable<string> GetPartitionTable() {
+            var lines = Bash.Execute($"echo -e print | {partedCommand}");
+            return lines;
+        }
+
+        public static IEnumerable<string> GetPartitionTable(string device) {
+            var lines = Bash.Execute($"echo -e \\\"select {device}{eCR}print\\\" | {partedCommand}");
+            return lines;
+        }
+    }
+}
