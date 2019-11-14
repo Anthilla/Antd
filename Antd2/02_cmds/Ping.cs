d4082e4e481f7d79481237a5837bb66687c6615f
zandam <damiano.zanardi@anthilla.com>
2019-11-14 15:46:13 +0100

    integrato comando ping

---
+++ Antd2/02_cmds/Ping.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Antd2.cmds {
+    public class Ping {
+
+        private const string pingCommand = "ping";
+
+        public static IEnumerable<string> Get(string destination, int count = 4) {
+            var result = Bash.Execute($"{pingCommand} -c {count} {destination}");
+            return result;
+        }
+    }
+}
