c218e221db58989e6a3cd0e3e9b589fb1c1eab84
zandam <damiano.zanardi@anthilla.com>
2019-11-14 15:44:52 +0100

    corretto script installazione mono dotnet

--- Antd2/Scripts/dotnet_install.sh
+++ Antd2/Scripts/dotnet_install.sh
@@ -1,3 +1,4 @@
+cd
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
 mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
 wget -q https://packages.microsoft.com/config/debian/9/prod.list
@@ -5,7 +6,7 @@ mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 apt-get update
-apt-get --yes --force-yes install mono-complete mono-roslyn
+apt-get --yes --force-yes install mono-complete 
 apt-get --yes --force-yes install apt-transport-https
 apt-get update
 apt-get --yes --force-yes install dotnet-sdk-3.0
