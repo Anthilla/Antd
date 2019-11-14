8b2110d8b7518bc2719da6ba2a0472cd0044e166
zandam <damiano.zanardi@anthilla.com>
2019-11-14 15:46:24 +0100

    aggiunto cmd per controllo status macchina

---
+++ Antd2/01_core/commands/CheckCommand.cs
@@ -0,0 +1,33 @@
+﻿using Antd2.cmds;
+using System;
+using System.Collections.Generic;
+
+namespace Antd2 {
+    public class CheckCommand {
+        public static readonly Dictionary<string, Action<string[]>> Options =
+            new Dictionary<string, Action<string[]>> {
+                {"os", Os },
+                {"internet", Internet },
+                {"dns", Dns },
+            };
+
+        private static void Os(string[] args) {
+            Console.WriteLine("  os:");
+            Console.WriteLine($"  {Uname.Get()}");
+        }
+
+        private static void Internet(string[] args) {
+            var destination = args.Length > 0 ? args[0] : "8.8.8.8";
+            Console.WriteLine($"  test ping: {destination}");
+            foreach (var line in Ping.Get(destination))
+                Console.WriteLine($"  {line}");
+        }
+
+        private static void Dns(string[] args) {
+            var destination = args.Length > 0 ? args[0] : "google.com";
+            Console.WriteLine($"  test ping: {destination}");
+            foreach (var line in Ping.Get(destination))
+                Console.WriteLine($"  {line}");
+        }
+    }
+}
