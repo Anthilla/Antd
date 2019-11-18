fee20e3fe382f85e382439e4fcb363d31395e836
zandam <damiano.zanardi@anthilla.com>
2019-11-18 11:54:08 +0100

    integrato comando dd

---
+++ Antd2/02_cmds/Dd.cs
@@ -0,0 +1,10 @@
+﻿namespace Antd2.cmds {
+    public class Dd {
+
+        private const string ddCommand = "dd";
+
+        public static void CopyBlocks(string sourceDevice, string destinationDevice, string blockStep = "8192K") {
+            Bash.Do($"{ddCommand} if={sourceDevice} of={destinationDevice} bs={blockStep}");
+        }
+    }
+}
