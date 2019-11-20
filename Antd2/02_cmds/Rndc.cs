54fa47a14bc9e181ebbef00d9313eabe98b22a08
zandam <damiano.zanardi@anthilla.com>
2019-11-20 11:08:36 +0100

    add command rndc

---
+++ Antd2/02_cmds/Rndc.cs
@@ -0,0 +1,16 @@
+﻿namespace Antd2.cmds {
+    public class Rndc {
+
+        private const string rndcCommand = "rndc";
+        private const string reconfigArg = "reconfig";
+        private const string reloadArg = "reload";
+
+        public static void Reconfig() {
+            Bash.Do($"{rndcCommand} {reconfigArg}");
+        }
+
+        public static void Reload() {
+            Bash.Do($"{rndcCommand} {reloadArg}");
+        }
+    }
+}
