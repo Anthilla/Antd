48445d56d63ff4d8131f1ac0c87c60ee76a74a86
zandam <damiano.zanardi@anthilla.com>
2019-11-20 11:07:59 +0100

    add commands kill and killall

---
+++ Antd2/02_cmds/Kill.cs
@@ -0,0 +1,15 @@
+﻿namespace Antd2.cmds {
+    public class Kill {
+
+        private const string killCommand = "/bin/kill";
+        private const string killallCommand = "/usr/bin/killall";
+
+        public static void Process(string pid) {
+            Bash.Do($"{killCommand} -9 {pid}");
+        }
+
+        public static void All(string processName) {
+            Bash.Do($"{killallCommand} {killCommand}");
+        }
+    }
+}
