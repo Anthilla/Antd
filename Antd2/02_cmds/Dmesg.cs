76b9abefb6c5840585eb200d61573cfd4ab8d0af
zandam <damiano.zanardi@anthilla.com>
2019-11-20 11:07:32 +0100

    add command dmesg

---
+++ Antd2/02_cmds/Dmesg.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Antd2.cmds {
+    public class Dmesg {
+
+        private const string dmesgCommand = "dmesg";
+
+        public static IEnumerable<string> GetLog() {
+            return Bash.Execute(dmesgCommand);
+        }
+    }
+}
