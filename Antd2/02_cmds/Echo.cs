3159c4bda8a50da98d2dafb6be15dd8c396c7a3d
zandam <damiano.zanardi@anthilla.com>
2019-11-20 11:07:41 +0100

    add command echo

---
+++ Antd2/02_cmds/Echo.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Antd2.cmds {
+    public class Echo {
+
+        private const string echoCommand = "echo";
+
+
+        /// <summary>
+        /// -c "echo {value} > {filePath}"
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="filePath"></param>
+        public static void PipeToFile(string value, string filePath) {
+            if (!File.Exists(filePath)) {
+                return;
+            }
+            Bash.Do($"{echoCommand} {value} > {filePath}");
+        }
+    }
+}
