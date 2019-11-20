8af6734ebbf6dae11ec505ffd6baff88927dd9b7
zandam <damiano.zanardi@anthilla.com>
2019-11-20 11:07:10 +0100

    add command df

---
+++ Antd2/02_cmds/Dhcpcd.cs
@@ -0,0 +1,10 @@
+﻿namespace Antd2.cmds {
+    public class Dhcpcd {
+
+        private const string dhcpcdCommand = "dhcpcd";
+
+        public static void Start(string networkAdapter) {
+            Bash.Do($"{dhcpcdCommand} {networkAdapter}");
+        }
+    }
+}
