1e586e45a8a07fa9d3fca0df52b373d34d003226
zandam <damiano.zanardi@anthilla.com>
2020-02-15 12:22:52 +0100

    gzip command integration

---
+++ Antd2/02_cmds/Gzip.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Antd2.cmds {
+    public class Gzip {
+
+        private const string gzipCommand = "gzip";
+
+        public static void CompressFile(string file) {
+            if (File.Exists(file))
+                Bash.Execute($"{gzipCommand} {file}");
+        }
+
+        public static void CompressRecursive(string directory) {
+            if (Directory.Exists(directory))
+                Bash.Execute($"{gzipCommand} -r {directory}");
+        }
+    }
+}
