eeb77955e12c4602204b7ee7f98fde0f851d72f0
zandam <damiano.zanardi@anthilla.com>
2020-02-24 10:19:48 +0100

    add find command

---
+++ Antd2/02_cmds/Find.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Antd2.cmds {
+    public class Find {
+
+        private const string findCommand = "find";
+
+        public static IEnumerable<string> File(string path, string pattern, string type = "f") {
+            var typeString = string.IsNullOrEmpty(type) ? "" : $" -type {type} ";
+            return Bash.Execute($"{findCommand} {path} {typeString} -name {pattern}");
+        }
+    }
+}
